2bfd9d5824e97cdcb77e3c17d73f96ca96c802b0
Lucas Mateus Venancio Rossi <Lucasmateusrossi1@gmail.com>
2024-01-23 17:24:20 -0300

    adicionando bootstrap

--- app.py
+++ app.py
@@ -1,35 +1,39 @@
-from flask import Flask ,render_template, request
+from flask import Flask ,render_template, request, url_for
 from random import randint 
 
-app= Flask(__name__)
-
-#@app.route("/" )
-#'''def index():
- #   variavel= "Game: adivinhe o número de 0 a 2 00:"
-    
-  #  if request.method=="GET":
-   #     return render_template("/html/home.html", variavel=variavel)
-    #else:  
-     #   comp = 0
-      #  comp= randint(1,200) 
-       # user = int(request.form.get("name"))
-        
-        #if user==comp :
-         #   return "<h1>Você acertou!!</h1>"
-        #else:
-         #   return "<h2>Você errou tente novamente</h2>" '''
-#def hello(nome):
-    #return f"<h1>Olá {nome}<h1>"
+app= Flask(__name__, static_folder="static")
 
 
 @app.route("/")
 def index():
     return render_template("/html/home.html")
 
- 
+
+@app.route("/login", methods = ["POST"])
+def login():
+    user= request.form.get("name")
+    keyword = request.form.get("password")                
+    if user == "Lucas" and keyword =="Bananas123":            
+        return f"<h1>Bem vindo {user} </h1>"
+    elif user !="Lucas" and keyword != "Bananas123":
+        erro = "VOCÊ DIGITOU A SENHA E O USUÁRIO INCORRETAMENTE"
+        return render_template("/html/home.html",erro = erro)
+    elif user != "Lucas":
+        erro = "VOCÊ DIGITOU O USUÁRIO INCORRETAMENTE"            
+        return render_template("/html/home.html",erro = erro)
+    else:
+        erro = "VOCÊ DIGITOU A SENHA INCORRETAMENTE"            
+        return render_template("/html/home.html",erro = erro)
+
+@app.route("/Produtos")
+def produtos():
+    return render_template("/html/produtos.html")
+
+
 @app.route("/<string:nome>")
 def error(nome):
     return render_template("/html/erro.html", name_page=nome)
+
 
 
 if __name__=="__main__":
