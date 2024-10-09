18348c571ad743ec52204acedc42fe259727747a
Lucas <lucas.venanciorossi@gmail.com>
2024-10-09 11:24:55 -0300

    now it's work

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 from app import cli 
 
+<<<<<<< HEAD
 
 
 if __name__=="__main__":
     cli(debug = True, port=5000)
+=======
+if __name__=="__main__":
+    cli(port=5000)
+>>>>>>> b534878 (now it's work)
